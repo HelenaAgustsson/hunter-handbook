f7754851a9787774bd2b8f44c76420e77aab4bc4
HF-Agustsson <43092712+HelenaAgustsson@users.noreply.github.com>
2023-03-08 17:14:34 +0000

    react task

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+const Footer = ()=>{
+    return (
+        <footer>
+            <p>Created by Helena Agustsson</p>
+            <a href="" >About</a>
+        </footer>
+        
+    )
+    
+}
+
+export default Footer
